8f06aac4b2390eaa5d3fe9604a5f808cf41c1fae
Gilbert Pellegrom <gilbert@pellegrom.me>
2018-12-03 11:50:53 +0000

    Don't exclude .gitignore_global

--- install.sh
+++ install.sh
@@ -105,7 +105,7 @@ install_dotfiles () {
 
   local overwrite_all=false backup_all=false skip_all=false
 
-  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '.*' -not -path '*.git*')
+  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '.*' -not -path '*.git')
   do
     dst="$HOME/$(basename $src)"
 	if [ $(basename $dst) != ".dotfiles" ]
