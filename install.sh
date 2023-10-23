25615c726beaf081ddd50195ccf00e9706e300a4
Gilbert Pellegrom <gilbert@pellegrom.me>
2023-10-23 12:20:13 +0100

    Switch to Homebrew for Zsh plugins

--- install.sh
+++ install.sh
@@ -120,17 +120,9 @@ install_dotfiles
 info "Configuring .gitignore_global..."
 git config --global core.excludesfile ~/.gitignore_global
 
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
-  info "Installing powerlevel10k..."
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-fi
-
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
-  info "Installing zsh-syntax-highlighting..."
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-fi
-
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-  info "Installing zsh-autosuggestions..."
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-fi
+info "Installing powerlevel10k..."
+brew install powerlevel10k
+info "Installing zsh-syntax-highlighting..."
+brew install zsh-syntax-highlighting
+info "Installing zsh-autosuggestions..."
+brew install zsh-autosuggestions
